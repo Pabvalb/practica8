f686b10ddf7cbe6f0d5f9caedcd923fb41da99c8
Pabvalb <pabvalb02@gmail.com>
2026-04-09 14:18:34 -0600

    Update programa3.c

--- programa3.c
+++ programa3.c
@@ -13,8 +13,11 @@ int main ()
     veces++;
     sum = sum + calif;
     printf("¿Desea sumar otra? S/N\n");
-    setbuf(stdin, NULL);       // limpia el buffer del teclado scanf("%c",&op);
+    setbuf(stdin, NULL);       // limpia el buffer del teclado 
+    scanf(" %c",&op);
     getchar();
   }
   while (op == 'S' || op == 's');
+  printf("El promedio de las calificaciones ingresadas es: %lf\n", sum/veces);
+  return 0; 
 }
